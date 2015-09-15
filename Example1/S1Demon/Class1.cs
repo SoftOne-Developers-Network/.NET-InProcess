5f9259f087ee8b188e2ed665dfbdc9f1428ad220
Giorgoulakis <giannis@giorgoulakis.com>
2015-09-15 11:42:16 +0300

    first commit

---
+++ Example1/S1Demon/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Softone;
+
+namespace S1Demon
+{
+    static public class Example : Object
+    {
+        static public XSupport XSupport;
+        static public Timer myTimer;
+        static public XTable myData;
+
+
+        static void timer1_Tick(object sender, EventArgs e)
+        {
+            myTimer.Enabled = false;
+            if (MessageBox.Show("Message form DLL. Press Cancel to Stop Timer", "SoftOne", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                myTimer.Enabled = true;
+        }
+
+        static public void Initialize()
+        {
+            myTimer = new Timer();
+            myTimer.Interval = 8000;
+            myTimer.Tick += timer1_Tick;
+            myTimer.Enabled = true;
+            myData = XSupport.GetSQLDataSet("Select code, Name from trdr where sodtype=13", null);
+        }
+    }
+
+
+
+    public class S1Init : TXCode
+    {
+        public override void Initialize()
+        {
+            Example.XSupport = XSupport;
+            Example.Initialize();
+            MessageBox.Show("Start Demon!");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Example2/NetExample2/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Softone;
+using System.Windows.Media;//
+
+namespace NetExample2
+{
+    public class S1Init : TXCode
+    {
+        public override void Initialize()
+        {
+            MessageBox.Show("dotNET DLL Loaded OK!");
+        }
+
+    }
+
+    [WorksOn("CUSTOMER/S")]
+    public class SCustomer : TXCode
+    {
+        public override void Initialize()
+        {
+        }
+
+        XTable t;
+        public override void BeforePost()
+        {
+            t = XModule.GetTable("TRDR");
+            MessageBox.Show((string)t[0, "CODE"]);
+            base.BeforePost();
+        }
+
+        public override void AfterPost()
+        {
+            base.AfterPost();
+        }
+
+    }
+
+    [WorksOn("CUSTOMER")]
+    public class Customer : TXCode
+    {
+        XTable CustTable;
+
+        private void NameChanged(object Sender, XEventArgs e)
+        {
+            MessageBox.Show(" Name Changed");
+        }
+
+        public override void Initialize()
+        {
+
+            CustTable = XModule.GetTable("TRDR");
+            XModule.SetEvent("ON_TRDR_NAME", NameChanged);
+            XModule.SetEvent("ON_TRDR_POST", NameChanged);
+
+
+            string s;
+            for (int i = 0; i < XModule.Params.Length; i++)
+            {
+                s = XModule.Params[i];
+                if (!String.IsNullOrEmpty(s))
+                {
+                    if ((s.Length > 4) && (s.StartsWith("FORM")))
+                    {
+                        MessageBox.Show(XModule.Params[i]);
+                    }
+                }
+            }
+        }
+    }
+}
